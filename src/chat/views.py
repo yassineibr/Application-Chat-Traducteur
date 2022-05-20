8d09b8681a12244ac6307e427e1909c3cd564676
yassineibr <yassineibr1@gmail.com>
2022-05-21 00:14:39 +0100

    chatrooms with websockets using channels

---
+++ src/chat/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html',{})
+
+
+def room(request, room_name):
+    return render(request, 'chatroom.html', {
+        'room_name' : room_name,
+    })
+
+def test(request):
+    return render(request, 'test.html',{})
